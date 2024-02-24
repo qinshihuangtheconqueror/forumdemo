1293ce24318b29b7ba0d96e13c5f9493cab14d61
Rody <huy.lh225976@sis.hust.edu.vn>
2024-02-24 16:29:48 +0700

    hi, first commit

---
+++ client/src/components/common/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">Green Guardian</h1>
+      <p class="lead">We &lt;3 to save your plants in the best statement!</p>
+      <hr class="my-4" />
+      <p className="lead">A disscussion forum for enthusiastic farmer and professors.</p>
+    </div>
+  );
+};
+
+export default Jumotron;
